6341ed85524188ba249cea29b0ad86a031432dcd
Ivan Mattie <ivan.mattie@squareinfinity.com>
2015-04-09 20:13:30 -0600

    Added Basic Mobile Support

--- app/views/master/master.blade.php
+++ app/views/master/master.blade.php
@@ -3,6 +3,7 @@
 <html>
     <head>
         <title>Pizza Rolls Power</title>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;user-scalable=no;">
         
         <!-- jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
