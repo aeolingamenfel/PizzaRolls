1439b30c18f095d946c59205823a22b9ca0b383d
Ivan Mattie <ivan.mattie@squareinfinity.com>
2015-04-09 21:28:37 -0600

    Started Using a Controller

---
+++ app/controllers/ApiController.php
@@ -0,0 +1,74 @@
+<?php
+
+class ApiController extends Controller {
+
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    protected function productComparison()
+    {
+        /*
+        * === ULTRA IMPORTANT VARIABLE, KEEP SAFE ===
+        */
+       $pricePerPizzaRoll = 0.087;
+       /*
+        * === ULTRA IMPORTANT VARIABLE ABOVE ===
+        */
+
+       $searchData = Input::get('search');
+
+       $urlSafeSearchData = urlencode($searchData);
+
+       $errorMessage = "";
+
+       for($x=0; $x<5; $x++){
+           try{
+               $rawData = file_get_contents('http://api.walmartlabs.com/v1/search?query=' . $urlSafeSearchData . '&format=json&apiKey=uyaq7zdc4rwmx48qp6kqpgv3');
+
+               $data = json_decode($rawData);
+
+               if($data->totalResults > 0){
+                   $items = $data->items;
+                   $firstItem = $items[0];
+                   $firstItem_arr = (array) $firstItem;
+
+                   //return var_dump($firstItem);
+
+                   $cost = $firstItem->salePrice;
+                   $pizzaRollCost = floor($cost / $pricePerPizzaRoll);
+
+                   $optionalData = array(
+                       "shortDescription" => "shortDescription",
+                       "image" => "thumbnailImage",
+                       "ratingImage" => "customerRatingImage"
+                   );
+
+                   $output = array(
+                       "raw" => $pizzaRollCost,
+                       "cost" => $cost,
+                       "productName" => $firstItem->name,
+                       "string" => "A " . $firstItem->name . " is worth " . $pizzaRollCost . " pizza rolls."
+                   );
+
+                   foreach($optionalData as $outputString=> $itemString){
+                       if(isset($firstItem_arr[$itemString])){
+                           $output[$outputString] = $firstItem_arr[$itemString];
+                       }else{
+                           $output[$outputString] = "";
+                       }
+                   }
+
+                   return PizzaRoller::RollJSON(1, "Found item! Amount of pizza rolls incoming.", PizzaRoller::ArrayToObject($output));
+               }else{
+                   return PizzaRoller::RollJSON(-1, "Could not find item.", null);
+               }
+           }catch(Exception $exception){
+               $errorMessage .= "\n" . $exception->getMessage();
+           }
+       }
+       return PizzaRoller::RollJSON(-1, $errorMessage, null);
+    }
+
+}
--- app/routes.php
+++ app/routes.php
@@ -1,80 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::get('/', function()
 {
     return View::make('index');
 });
 
-Route::get('/comparison/product', function(){
-    /*
-     * === ULTRA IMPORTANT VARIABLE, KEEP SAFE ===
-     */
-    $pricePerPizzaRoll = 0.087;
-    /*
-     * === ULTRA IMPORTANT VARIABLE ABOVE ===
-     */
-    
-    $searchData = Input::get('search');
-    
-    $urlSafeSearchData = urlencode($searchData);
-    
-    $errorMessage = "";
-    
-    for($x=0; $x<5; $x++){
-        try{
-            $rawData = file_get_contents('http://api.walmartlabs.com/v1/search?query=' . $urlSafeSearchData . '&format=json&apiKey=uyaq7zdc4rwmx48qp6kqpgv3');
-
-            $data = json_decode($rawData);
-
-            if($data->totalResults > 0){
-                $items = $data->items;
-                $firstItem = $items[0];
-                $firstItem_arr = (array) $firstItem;
-
-                //return var_dump($firstItem);
-
-                $cost = $firstItem->salePrice;
-                $pizzaRollCost = floor($cost / $pricePerPizzaRoll);
-                
-                $optionalData = array(
-                    "shortDescription" => "shortDescription",
-                    "image" => "thumbnailImage",
-                    "ratingImage" => "customerRatingImage"
-                );
-
-                $output = array(
-                    "raw" => $pizzaRollCost,
-                    "cost" => $cost,
-                    "productName" => $firstItem->name,
-                    "string" => "A " . $firstItem->name . " is worth " . $pizzaRollCost . " pizza rolls."
-                );
-                
-                foreach($optionalData as $outputString=> $itemString){
-                    if(isset($firstItem_arr[$itemString])){
-                        $output[$outputString] = $firstItem_arr[$itemString];
-                    }else{
-                        $output[$outputString] = "";
-                    }
-                }
-
-                return PizzaRoller::RollJSON(1, "Found item! Amount of pizza rolls incoming.", PizzaRoller::ArrayToObject($output));
-            }else{
-                return PizzaRoller::RollJSON(-1, "Could not find item.", null);
-            }
-        }catch(Exception $exception){
-            $errorMessage .= "\n" . $exception->getMessage();
-        }
-    }
-    return PizzaRoller::RollJSON(-1, $errorMessage, null);
-});
+Route::get('/comparison/product', 'ApiController@productComparison');
